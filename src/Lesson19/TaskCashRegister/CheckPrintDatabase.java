bfbe86669ed515c2982d35acd4fb928afbfc5051
Роман <legrom@ukr.net>
2017-12-02 01:47:44 +0200

    Task 19.01 code review 2

--- src/Lesson19/TaskCashRegister/CheckPrintDatabase.java
+++ src/Lesson19/TaskCashRegister/CheckPrintDatabase.java
@@ -5,18 +5,30 @@ import java.time.LocalDate;
 
 public class CheckPrintDatabase implements CheckPrintable {
     @Override
-    public void saveCheck(String text) throws IOException {
-        double sum = MenuCashRegister.getSum();
-        LocalDate date = LocalDate.now();
-        File file = new File("C:\\Users\\Роман\\Desktop\\database.txt");
-        PrintWriter writer = new PrintWriter(new FileWriter(file,true));
-        writer.println("---------------------------------------------------");
-        writer.println("            < ЧЕК ОПЛАТИ > ||database||            ");
-        writer.println("---------------------------------------------------");
-        writer.println("  "+text+"                                         ");
-        writer.println("  СУМА ОПЛАТИ: " + sum+"                           ");
-        writer.println("  "+date+"                                         ");
-        writer.println("---------------------------------------------------");
-        writer.close();
+    public void saveCheck(String text) {
+        File file =null;
+        PrintWriter writer = null;
+        try {
+            double sum = MenuCashRegister.getSum();
+            LocalDate date = LocalDate.now();
+            file = new File("C:\\Users\\Роман\\Desktop\\database.txt");
+            writer = new PrintWriter(new FileWriter(file,true));
+            writer.println("---------------------------------------------------");
+            writer.println("            < ЧЕК ОПЛАТИ > ||database||            ");
+            writer.println("---------------------------------------------------");
+            writer.println("  "+text+"                                         ");
+            writer.println("  СУМА ОПЛАТИ: " + sum+"                           ");
+            writer.println("  "+date+"                                         ");
+            writer.println("---------------------------------------------------");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                writer.close();
+            } catch (Exception e) {
+                System.out.println("Exception while file closing");
+            }
+        }
     }
 }
---
+++ src/Lesson20/Task_WorkWithHandlingExceptions/Menu.java
@@ -0,0 +1,55 @@
+package Lesson20.Task_WorkWithHandlingExceptions;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Menu {
+    private static Scanner scanner = new Scanner(System.in);
+    private static int choice;
+
+    public static void showMenu() throws InputMismatchException {
+
+        showMainMenu();
+
+        do {
+            System.out.print("--> ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    CreateCircle.newCircle();
+                    break;
+                case 2:
+                    CircleList.showArea();
+                    break;
+                case 0:
+                    System.out.println("Exit...");
+                    break;
+                default:
+                    System.out.println("Not the right choice!");
+                    Menu.showMainMenu();
+            }
+
+        } while (choice != 0);
+        System.exit(0);
+    }
+
+    public static void showMainMenu() {
+        System.out.println("****************************************************");
+        System.out.println("*                      MENU:                       *");
+        System.out.println("****************************************************");
+        System.out.println("* 1. ДОДАТИ КРУГ ДО СПИСКУ:                        *");
+        System.out.println("* 2. РОЗРАХУВАТИ ПЛОЩУ КРУГА:                      *");
+        System.out.println("* 0. вийти із меню:                                *");
+        System.out.println("****************************************************");
+    }
+
+    public static void showArea() {
+        System.out.println("****************************************************");
+        System.out.println("*                      AREA:                       *");
+        System.out.println("****************************************************");
+        System.out.println("* 1. розрахувати площу першого круга:              *");
+        System.out.println("* 2. розрахувати площу другого круга:              *");
+        System.out.println("****************************************************");
+    }
+}
